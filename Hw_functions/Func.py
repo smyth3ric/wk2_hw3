48b3bc610ecfe983b38d9d8338e2bd3a803fdf0e
eric smith <smyth.3ric@gmail.com>
2022-10-06 12:11:45 -0400

    first commit

---
+++ Hw_functions/Func.py
@@ -0,0 +1,14 @@
+# 1) Has a function to calculate the square footage of a house
+# Reminder of Formula: Length X Width == Area
+
+def area(l,w):
+    print(l*w)
+
+
+# 2) Has a function to calculate the circumference of a circle
+
+def cir(r):
+    print((2*3.14159265359)*r)
+
+
+# Program in Jupyter Notebook should take in user input and use imported functions to calculate a circle's circumference or a houses square footage
